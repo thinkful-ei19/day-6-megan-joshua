23e8a8ba9f7b200fa23ef8f774f3cb04cd22c465
Joshua Hutchinson <jthutch94@gmail.com>
2018-02-26 14:28:46 -0500

    update-to-Item.create

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,6 +1,6 @@
 
 'use strict';
-/* global store, cuid */
+/* global store, Item, cuid */
 
 // eslint-disable-next-line no-unused-vars
 const shoppingList = (function(){
@@ -58,7 +58,13 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try{
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+    } catch(error) {
+      console.log('Cannot add item: ' + error.message);
+    }
   }
   
   function handleNewItemSubmit() {
